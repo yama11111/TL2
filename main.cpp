459dd20acb69731bd2e52da012d85206140e45e1
YamanakaRui <k021g1123@g.neec.ac.jp>
2023-12-11 14:33:22 +0900

    2_3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() 
+int main(int argc, char* argv[])
 {
-
-	printf("Hello, World!\n");
+	for (size_t i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		printf("\n");
+	}
 
 	system("pause");
+
 	return 0;
 }
