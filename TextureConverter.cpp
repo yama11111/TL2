7c9aaadd3c7974c6cf3800a202bb97f031528a4c
YamanakaRui <k021g1123@g.neec.ac.jp>
2024-01-16 11:19:46 +0900

    02_05まで

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -2,21 +2,35 @@
 #include <windows.h>
 
 using YGame::TextureConverter;
+using namespace DirectX;
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
 	// ファイル読み込み
-	LoadWICTextureFromFile(filePath);
+	TextureData data = LoadWICTextureFromFile(filePath);
 
-	// 書き出し(DDS)
+	// 書き出し (DDS)
+	SaveDDSTextureToFile(data);
 }
 
-void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
+TextureConverter::TextureData TextureConverter::LoadWICTextureFromFile(
+	const std::string& filePath)
 {
+	TextureData data;
+
 	// ファイルパス変換
 	std::wstring wFilePath = ConvertMultiByteStringToWideString(filePath);
 
+	// 読み込み (WIC)
+	HRESULT hr = LoadFromWICFile(
+		wFilePath.c_str(), WIC_FLAGS_NONE, 
+		&data.metadata, data.scratchImg);
+	assert(SUCCEEDED(hr));
 
+	// ファイルパス分解
+	SeparateFilePath(filePath, data.directoryPath, data.fileName, data.fileExt);
+
+	return data;
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
@@ -34,3 +48,72 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::str
 
 	return result;
 }
+
+void TextureConverter::SeparateFilePath(
+	const std::string& filePath, 
+	std::string& directoryPath, 
+	std::string& fileName, 
+	std::string& fileExt)
+{
+	size_t pos1 = 0;
+	std::string exceptExt;
+
+	// '.' があるなら
+	pos1 = filePath.rfind('.');
+	if (pos1 != std::string::npos)
+	{
+		// '.' より後ろを抜き出す (ext)
+		fileExt = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else
+	{
+		fileExt = "";
+		exceptExt = filePath;
+	}
+
+	// '\\' があるなら
+	pos1 = exceptExt.rfind('\\');
+	if (pos1 != std::string::npos)
+	{
+		// '\\' より前を抜き出す (directory)
+		directoryPath = exceptExt.substr(0, pos1 + 1);
+		
+		// '\\' より後ろを抜き出す (fileName)
+		fileName = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		
+		return;
+	}
+
+	// '/' があるなら
+	pos1 = exceptExt.rfind('/');
+	if (pos1 != std::string::npos)
+	{
+		// '/' より前を抜き出す (directory)
+		directoryPath = exceptExt.substr(0, pos1 + 1);
+
+		// '/' より後ろを抜き出す (fileName)
+		fileName = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+
+		return;
+	}
+
+	directoryPath = "";
+	fileName = exceptExt;
+}
+
+void TextureConverter::SaveDDSTextureToFile(const TextureData& texData)
+{
+	DirectX::TexMetadata metadata = texData.metadata;
+	metadata.format = MakeSRGB(metadata.format);
+
+	std::wstring filePath = 
+		ConvertMultiByteStringToWideString(
+			texData.directoryPath + texData.fileName + ".dds");
+
+	HRESULT hr = SaveToDDSFile(
+		texData.scratchImg.GetImages(), texData.scratchImg.GetImageCount(),
+		metadata, DDS_FLAGS_NONE, filePath.c_str());
+	assert(SUCCEEDED(hr));
+}
+
