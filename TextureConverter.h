38d51d5db1814e62f7689eabb5bdab136bebbc09
YamanakaRui <k021g1123@g.neec.ac.jp>
2024-01-16 11:16:34 +0900

    02_06まで

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <string>
+#include <DirectXTex.h>
 
 namespace YGame
 {
@@ -12,15 +13,32 @@ namespace YGame
 		/// WIC → DDS変換
 		/// </summary>
 		/// <param name="filePath"> : ファイルパス</param>
-		void ConvertTextureWICToDDS(const std::string& filePath);
+		static void ConvertTextureWICToDDS(const std::string& filePath);
 
+	private:
+
+		/// <summary>
+		/// テクスチャデータ構造体
+		/// </summary>
+		struct TextureData
+		{
+			// 画像データ
+			DirectX::TexMetadata metadata{};
+			DirectX::ScratchImage scratchImg{};
+			
+			// ファイルパス
+			std::string directoryPath;
+			std::string fileName;
+			std::string fileExt;
+		};
+	
 	private:
 
 		/// <summary>
 		/// テクスチャファイル読み込み
 		/// </summary>
 		/// <param name="filePath"> : ファイルパス</param>
-		void LoadWICTextureFromFile(const std::string& filePath);
+		static TextureData LoadWICTextureFromFile(const std::string& filePath);
 
 		/// <summary>
 		/// マルチバイト文字列 → ワイド文字列 変換
@@ -28,6 +46,25 @@ namespace YGame
 		/// <param name="mString"> : マルチバイト文字列</param>
 		/// <returns>ワイド文字列</returns>
 		static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+		/// <summary>
+		/// ファイルパス分解
+		/// </summary>
+		/// <param name="filePath"> : ファイルパス</param>
+		/// <param name="directoryPath"> : 受け取り用 ディレクトリパス</param>
+		/// <param name="fileName"> : 受け取り用 ファイル名</param>
+		/// <param name="fileExt"> : 受け取り用 ファイル拡張子</param>
+		static void SeparateFilePath(
+			const std::string& filePath,
+			std::string& directoryPath,
+			std::string& fileName,
+			std::string& fileExt);
+
+		/// <summary>
+		/// DDSテクスチャとしてファイル書き出し
+		/// </summary>
+		/// <param name="texData"> : 画像データ</param>
+		static void SaveDDSTextureToFile(const TextureData& texData);
 
 	};
 }
